9d087d518b5d46e5363cbea40bba417779577d0a
Reehana D R <93122048+ReehanaDR@users.noreply.github.com>
2022-12-25 17:47:39 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+
+function f1(){
+    var ele=document.getElementById("productname").style.backgroundColor="white";
+}
+
+function f2(){
+    var ele=document.getElementById("original").style.backgroundColor="white";
+}
+function f3(){
+    var ele=document.getElementById("sale").style.backgroundColor="white";
+}
+function f4(){
+    var ele=document.getElementById("product").style.backgroundColor="white";
+}
+function f5(){
+    var ele=document.getElementById("text").style.backgroundColor="white";
+}
